59f14febc3a93c3187d5b0f30d07bc0b2cef805c
RANI SINGH <166839448+RaniSingh1203@users.noreply.github.com>
2026-02-11 17:38:15 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -7,5 +7,8 @@ export const pool = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
