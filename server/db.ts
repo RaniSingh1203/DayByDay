a4b58230616a60c23cbd9a87e5b1bbe59ff984d8
RANI SINGH <166839448+RaniSingh1203@users.noreply.github.com>
2026-02-12 09:50:53 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,7 +1,8 @@
 import mysql from 'mysql2/promise'
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST,                 
+  port: Number(process.env.DB_PORT) || 3306, 
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
