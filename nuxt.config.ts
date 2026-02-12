b83385177e49e7718f7e41ac4d1728ef7b5e7335
RANI SINGH <166839448+RaniSingh1203@users.noreply.github.com>
2026-02-12 14:56:18 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,14 +5,7 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   runtimeConfig: {
     jwtSecret: process.env.JWT_SECRET,
-      dbHost: process.env.DB_HOST,
-    dbPort: process.env.DB_PORT,
-    dbUser: process.env.DB_USER,
-    dbPassword: process.env.DB_PASSWORD,
-    dbName: process.env.DB_NAME,
-
-    // Public variables (if needed)
-    public: {}
+    
   },
   modules: ['@pinia/nuxt'],
   css: ['./app/assets/css/main.css','~/assets/css/responsive.css'],
