9708a99fd9812d3f19f8fe32c21c6dcb85c79828
RANI SINGH <166839448+RaniSingh1203@users.noreply.github.com>
2026-02-12 12:11:33 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,18 @@
 import mysql from 'mysql2/promise'
 
 export const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),   // Important for Railway
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   connectTimeout: 10000,
+
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false   // Required for Railway
   }
 })
