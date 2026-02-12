628f881a8d542390403d68252e6afc418fcc0d58
RANI SINGH <166839448+RaniSingh1203@users.noreply.github.com>
2026-02-12 12:18:45 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,17 @@
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   runtimeConfig: {
     jwtSecret: process.env.JWT_SECRET
+      dbHost: process.env.DB_HOST,
+    dbPort: process.env.DB_PORT,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME,
+
+    // Public variables (if needed)
+    public: {}
   },
   modules: ['@pinia/nuxt'],
   css: ['./app/assets/css/main.css','~/assets/css/responsive.css'],
