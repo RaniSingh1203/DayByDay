c5eaea244128a0457abbcf7c775bd39a14eea0a5
RANI SINGH <166839448+RaniSingh1203@users.noreply.github.com>
2026-02-12 14:41:30 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -3,22 +3,31 @@ import { useRuntimeConfig } from '#imports'
 
 let pool: mysql.Pool
 
-export const getPool = () => {
-  if (!pool) {
-    const config = useRuntimeConfig()
+function createPool() {
+  const config = useRuntimeConfig()
 
-    pool = mysql.createPool({
-      host: config.dbHost,
-      port: Number(config.dbPort),
-      user: config.dbUser,
-      password: config.dbPassword,
-      database: config.dbName,
-      ssl: {
-        rejectUnauthorized: false
-      },
-      connectionLimit: 10
-    })
-  }
-
-  return pool
+  return mysql.createPool({
+    host: config.dbHost,
+    port: Number(config.dbPort),
+    user: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbName,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
 }
+
+export const pool = {
+  execute: async (query: string, params?: any[]) => {
+    if (!poolInstance) {
+      poolInstance = createPool()
+    }
+    return poolInstance.execute(query, params)
+  }
+}
+
+let poolInstance: mysql.Pool
